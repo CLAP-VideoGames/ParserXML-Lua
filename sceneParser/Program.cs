d29162e4c511b7d0ac27d1fba1635844e510b6a1
Joseda <jrave@ucm.es>
2022-03-29 17:50:19 +0200

    Base of parser. Reads a xml file and creates file parsed

---
+++ sceneParser/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Xml;
+using System.IO;
+
+namespace sceneParser {
+    class Program{
+        static void Main(string[] args) {
+            string writeFile = args[0];
+            int n = args.Length;
+            StreamWriter lua = new StreamWriter(writeFile);
+            string readFile = args[1];
+            XmlTextReader xtr = new XmlTextReader(readFile);
+            lua.WriteLine("map = {");
+            char c = ' ';
+            string numberIndentations = new string (c, 4);
+            while (xtr.Read()){
+
+                if (xtr.HasAttributes) {
+                    lua.WriteLine(numberIndentations + xtr.Name + " Attribute");
+                    for (int i = 0; i < xtr.AttributeCount; i++)
+                    {
+                        xtr.MoveToAttribute(i);
+                        lua.WriteLine(numberIndentations+ "Nam: " + xtr.Name + ", Value: " + xtr.Value);
+                    }
+                    xtr.MoveToElement();
+                }
+                //if (xtr.NodeType== XmlNodeType.Element && xtr.Name == "Name"){
+                //    Console.WriteLine("he");
+                //}
+                //switch (reader.NodeType)
+                //{
+                //    case XmlNodeType.Element: // The node is an element.
+                //        Console.Write("<" + reader.Name);
+                //        Console.WriteLine(">");
+                //        break;
+                //    case XmlNodeType.Text: //Display the text in each element.
+                //        Console.WriteLine(reader.Value);
+                //        break;
+                //    case XmlNodeType.EndElement: //Display the end of the element.
+                //        Console.Write("</" + reader.Name);
+                //        Console.WriteLine(">");
+                //        break;
+                //}
+            }
+
+            lua.WriteLine("}");
+            xtr.Close();
+            lua.Close();
+        }
+    }
+}

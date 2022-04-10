fa34c5662a27889fba78f1baac8d9a6ec6fc78a4
Joseda <jrave@ucm.es>
2022-04-10 14:03:07 +0200

    parser base done. Functional

---
+++ program/map.lua
@@ -0,0 +1,59 @@
+mapEntities = {
+    cubo = {
+        Transform = {
+            position = {  -3.500000 ,1.500000 ,-0.000000 },
+            rotation = {  0.923880 ,-0.000000 ,0.000000 ,-0.382683 },
+            scale = {  1.000000 ,1.000000 ,1.000000 },
+        },
+        GameObjectType = {
+            Character
+        },
+        RigidBody = {
+            Type = Dynamic,
+            Collider = Box,
+            Mass = 1.0,
+            isTrigger = false
+        },
+        MeshRenderer = {
+            MeshFile = "cubo.mesh",
+        }
+    },
+    suelo = {
+        Transform = {
+            position = {  -3.500000 ,-0.500000 ,0.000000 },
+            rotation = {  0.923880 ,-0.000000 ,0.000000 ,-0.382683 },
+            scale = {  1.000000 ,1.000000 ,1.000000 },
+        },
+        GameObjectType = {
+            Map
+        },
+        RigidBody = {
+            Type = Static,
+             Collider = Box,
+             Mass = 1.0,
+             isTrigger = false
+        },
+        MeshRenderer = {
+            MeshFile = "suelo.mesh",
+        }
+    },
+    detalle = {
+        Transform = {
+            position = {  -2.322678 ,0.085155 ,0.000000 },
+            rotation = {  0.923880 ,-0.000000 ,0.000000 ,-0.382683 },
+            scale = {  1.000000 ,1.000000 ,1.000000 },
+        },
+        GameObjectType = {
+            Map
+        },
+        RigidBody = {
+            Type = Static,
+             Collider = Box,
+             Mass = 1.0,
+             isTrigger = false
+        },
+        MeshRenderer = {
+            MeshFile = "detalle.mesh",
+        }
+    },
+}
--- sceneParser/Program.cs
+++ sceneParser/Program.cs
@@ -14,39 +14,13 @@ namespace sceneParser {
             string numberIndentations = new string (c, nSpaces);
 
             while (xtr.Read()){
-                if (xtr.HasAttributes) {
-                    if (xtr.Name == "xml") continue;
-                    else if (xtr.Name == "node"){
-                        xtr.MoveToNextAttribute();
-                        //Name
-                        lua.WriteLine(numberIndentations + xtr.Value + " = {");
-                        processTransform(ref lua, ref xtr, ref numberIndentations, ref nSpaces, c);
-
-                        //xtr.ReadEndElement();
-                        //int l = xtr.AttributeCount;
-
-
-                        break;
+                if (xtr.NodeType == XmlNodeType.Element){
+                    if (xtr.HasAttributes) {
+                        if (xtr.Name == "xml") continue;
+                        else if (xtr.Name == "node") {
+                            processEntity(ref lua, ref xtr, ref numberIndentations, ref nSpaces, c);
+                        }
                     }
-
-                    //if (xtr.NodeType== XmlNodeType.Element && xtr.Name == "Name"){
-                    //    Console.WriteLine("he");
-                    //}
-                    //switch (reader.NodeType)
-                    //{
-                    //    case XmlNodeType.Element: // The node is an element.
-                    //        Console.Write("<" + reader.Name);
-                    //        Console.WriteLine(">");
-                    //        break;
-                    //    case XmlNodeType.Text: //Display the text in each element.
-                    //        Console.WriteLine(reader.Value);
-                    //        break;
-                    //    case XmlNodeType.EndElement: //Display the end of the element.
-                    //        Console.Write("</" + reader.Name);
-                    //        Console.WriteLine(">");
-                    //        break;
-                    //}
-                    
                 }
             }
             lua.WriteLine("}");
@@ -54,33 +28,82 @@ namespace sceneParser {
             lua.Close();
         }
 
-        static void processTransform(ref StreamWriter lua, ref XmlTextReader xtr, ref string numberIndentations, ref int nSpaces, char c){
+        static void processEntity(ref StreamWriter lua, ref XmlTextReader xtr, ref string numberIndentations, ref int nSpaces, char c){
+            xtr.MoveToNextAttribute();
+            //Name
+            lua.WriteLine(numberIndentations + xtr.Value + " = {");
             nSpaces += 4;
             numberIndentations = new string(c, nSpaces);
+            processTransform(ref lua, ref xtr, ref numberIndentations, ref nSpaces, c);
+            readNextElement(ref xtr);
+            processComponents(ref lua, ref xtr, ref numberIndentations, ref nSpaces, c);
+            readNextElement(ref xtr);
+            processMeshData(ref lua, ref xtr, ref numberIndentations, ref nSpaces, c);
+            nSpaces -= 4;
+            numberIndentations = new string(c, nSpaces);
+            lua.WriteLine(numberIndentations + "},");
+        }
+
+        static void processTransform(ref StreamWriter lua, ref XmlTextReader xtr, ref string numberIndentations, ref int nSpaces, char c){
             lua.WriteLine(numberIndentations + "Transform = {");
             nSpaces += 4;
             numberIndentations = new string(c, nSpaces);
             int numberTransform = 3;
 
             for (int j = 0; j < numberTransform; j++){
-                xtr.Read();
-                xtr.Read();
+                readNextElement(ref xtr);
                 lua.Write(numberIndentations + xtr.Name + " = { ");
 
                 xtr.MoveToAttribute(0);
                 lua.Write(" " + xtr.Value + " ");
-                for (int i = 1; i < xtr.AttributeCount; i++)
-                {
+                for (int i = 1; i < xtr.AttributeCount; i++){
                     xtr.MoveToAttribute(i);
                     lua.Write("," + xtr.Value + " ");
                 }
                 lua.WriteLine("},");
             }
 
-            nSpaces -= 5;
+            nSpaces -= 4;
             numberIndentations = new string(c, nSpaces);
 
-            lua.WriteLine(numberIndentations + " },");
+            lua.WriteLine(numberIndentations + "},");
+        }
+
+        static void processComponents(ref StreamWriter lua, ref XmlTextReader xtr, ref string numberIndentations, ref int nSpaces, char c) {
+            do readNextElement(ref xtr);
+            while (xtr.GetAttribute("type") == "IDPropertyGroup");
+
+            while(xtr.NodeType != XmlNodeType.EndElement){
+                lua.WriteLine(numberIndentations + xtr.GetAttribute("name") + " = {");
+                nSpaces += 4;
+                numberIndentations = new string(c, nSpaces);
+
+                string[] data = xtr.GetAttribute("data").Split(',');
+                for (int i = 0; i < data.Length - 1; i++)
+                    lua.WriteLine(numberIndentations + data[i] + ",");
+                
+                lua.WriteLine(numberIndentations + data[data.Length - 1]);
+                nSpaces -= 4;
+                numberIndentations = new string(c, nSpaces);
+                lua.WriteLine(numberIndentations + "},");
+                readNextElement(ref xtr);
+            }
+        }
+    
+        static void readNextElement(ref XmlTextReader xtr) {
+            do{
+                xtr.Read();
+            } while (xtr.NodeType != XmlNodeType.Element && xtr.NodeType != XmlNodeType.EndElement);
+        }
+
+        static void processMeshData(ref StreamWriter lua, ref XmlTextReader xtr, ref string numberIndentations, ref int nSpaces, char c){
+            lua.WriteLine(numberIndentations + "MeshRenderer = {");
+            nSpaces += 4;
+            numberIndentations = new string(c, nSpaces);
+            lua.WriteLine(numberIndentations + "MeshFile = \"" + xtr.GetAttribute("meshFile") + "\",");
+            nSpaces -= 4;
+            numberIndentations = new string(c, nSpaces);
+            lua.WriteLine(numberIndentations + "}");
         }
     }
 }

3eebe6bd7e36c76c65d51edbd36a938a4b0eea0c
JosedaMachine <jrave@ucm.es>
2022-04-23 18:27:54 +0200

    Now is working

--- sceneParser/Program.cs
+++ sceneParser/Program.cs
@@ -1,62 +1,41 @@
 ﻿using System;
 using System.Xml;
 using System.IO;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Numerics;
 
 namespace sceneParser {
-    
     class Program{
-        static string materialName;
+
         static void Main(string[] args) {
             StreamWriter lua = new StreamWriter(args[0]);
             XmlTextReader xtr = new XmlTextReader(args[1]);
-            lua.WriteLine("scene = {");
+            lua.WriteLine("mapEntities = {");
             char c = ' ';
             int nSpaces = 4;
             string numberIndentations = new string (c, nSpaces);
-
-            List<string> entidades = new List<string>();
 
             while (xtr.Read()){
                 if (xtr.NodeType == XmlNodeType.Element){
                     if (xtr.HasAttributes) {
                         if (xtr.Name == "xml") continue;
                         else if (xtr.Name == "node") {
-                            processEntity(ref lua, ref xtr, ref numberIndentations, ref nSpaces, c, ref entidades);
+                            processEntity(ref lua, ref xtr, ref numberIndentations, ref nSpaces, c);
                         }
                     }
                 }
             }
             lua.WriteLine("}");
-
-            lua.WriteLine("");
-            lua.Write("entities = {");
-            for (int i = 0; i < entidades.Count - 1; i++)
-                lua.Write("\"" + entidades[i] + "\" ,");
-            lua.WriteLine("\"" + entidades[entidades.Count - 1] + "\" }");
-            lua.Write("\n");
-            if (args.Length == 3){
-                StreamReader codeLua = new StreamReader(args[2]);
-                string code;
-                while ((code = codeLua.ReadLine())  != null)
-                    lua.WriteLine(code);
-            }
             xtr.Close();
             lua.Close();
         }
 
-        static void processEntity(ref StreamWriter lua, ref XmlTextReader xtr, ref string numberIndentations, ref int nSpaces, char c, ref List<string> ent){
+        static void processEntity(ref StreamWriter lua, ref XmlTextReader xtr, ref string numberIndentations, ref int nSpaces, char c){
             xtr.MoveToNextAttribute();
             //Name
-            ent.Add(xtr.Value);
             lua.WriteLine(numberIndentations + xtr.Value + " = {");
             nSpaces += 4;
             numberIndentations = new string(c, nSpaces);
             processTransform(ref lua, ref xtr, ref numberIndentations, ref nSpaces, c);
             readNextElement(ref xtr);
-            materialName = null;
             processComponents(ref lua, ref xtr, ref numberIndentations, ref nSpaces, c);
             readNextElement(ref xtr);
             processMeshData(ref lua, ref xtr, ref numberIndentations, ref nSpaces, c);
@@ -73,35 +52,7 @@ namespace sceneParser {
 
             for (int j = 0; j < numberTransform; j++){
                 readNextElement(ref xtr);
-                lua.Write(numberIndentations + xtr.Name + " = \"");
-
-                if (xtr.Name == "rotation"){
-                    Quaternion q;
-
-                    NumberFormatInfo provider = new NumberFormatInfo();
-                    provider.NumberDecimalSeparator = ".";
-
-                    xtr.MoveToAttribute(0);
-                    q.W = (float) Convert.ToDouble(xtr.Value, provider);
-                    xtr.MoveToAttribute(1);
-                    q.X = (float) Convert.ToDouble(xtr.Value, provider);
-                    xtr.MoveToAttribute(2);
-                    q.Y = (float) Convert.ToDouble(xtr.Value, provider);
-                    xtr.MoveToAttribute(3);
-                    q.Z = (float) Convert.ToDouble(xtr.Value, provider);
-                    
-                    Vector3 rotation = ToEulerAngles(q);
-                    rotation = toRadians(rotation);
-
-
-                    NumberFormatInfo nfi = new NumberFormatInfo();
-                    nfi.NumberDecimalSeparator = ".";
-
-                    lua.Write(" " + rotation.X.ToString(nfi) + " ," + rotation.Y.ToString(nfi) + " ," + rotation.Z.ToString(nfi) +  " ");
-                    lua.WriteLine("\",");
-
-                    continue;
-                }
+                lua.Write(numberIndentations + xtr.Name + " = { ");
 
                 xtr.MoveToAttribute(0);
                 lua.Write(" " + xtr.Value + " ");
@@ -109,7 +60,7 @@ namespace sceneParser {
                     xtr.MoveToAttribute(i);
                     lua.Write("," + xtr.Value + " ");
                 }
-                lua.WriteLine("\",");
+                lua.WriteLine("},");
             }
 
             nSpaces -= 4;
@@ -123,40 +74,18 @@ namespace sceneParser {
             while (xtr.GetAttribute("type") == "IDPropertyGroup");
 
             while(xtr.NodeType != XmlNodeType.EndElement){
-                if(xtr.GetAttribute("name") == "Enabled"){
-                    lua.Write(numberIndentations + xtr.GetAttribute("name") + " = ");
-                    string[] data_ = xtr.GetAttribute("data").Split(';');
+                lua.WriteLine(numberIndentations + xtr.GetAttribute("name") + " = {");
+                nSpaces += 4;
+                numberIndentations = new string(c, nSpaces);
 
-                    for (int i = 0; i < data_.Length; i++)
-                        lua.WriteLine(data_[i] + ",");
-                }else if (xtr.GetAttribute("name") == "Material"){
-                    materialName = new String(xtr.GetAttribute("data"));
-                }
-                else
-                {
-                    lua.WriteLine(numberIndentations + xtr.GetAttribute("name") + " = {");
-                    nSpaces += 4;
-                    numberIndentations = new string(c, nSpaces);
-
-                    string[] data = xtr.GetAttribute("data").Split(';');
-                    for (int i = 0; i < data.Length - 1; i++){
-                        if (data[i].Split('=')[0] == "Dimensions "){
-                            int index = data[i].IndexOf('\"') + 1;
-                            int length = data[i].Length - 2 - index;
-                            string aux = data[i].Substring(index, length);
-                            string[] dimensionsData = aux.Split(',');
-                            string dimensions = "\"" + dimensionsData[0] + ", " + dimensionsData[2] + ", " + dimensionsData[1] + "\"";
-                            lua.WriteLine(numberIndentations + "Dimensions = " + dimensions + ",");
-                        }
-                        else
-                            lua.WriteLine(numberIndentations + data[i] + ",");
-                    }
+                string[] data = xtr.GetAttribute("data").Split(',');
+                for (int i = 0; i < data.Length - 1; i++)
+                    lua.WriteLine(numberIndentations + data[i] + ",");
                 
-                    lua.WriteLine(numberIndentations + data[data.Length - 1]);
-                    nSpaces -= 4;
-                    numberIndentations = new string(c, nSpaces);
-                    lua.WriteLine(numberIndentations + "},");
-                }
+                lua.WriteLine(numberIndentations + data[data.Length - 1]);
+                nSpaces -= 4;
+                numberIndentations = new string(c, nSpaces);
+                lua.WriteLine(numberIndentations + "},");
                 readNextElement(ref xtr);
             }
         }
@@ -172,49 +101,9 @@ namespace sceneParser {
             nSpaces += 4;
             numberIndentations = new string(c, nSpaces);
             lua.WriteLine(numberIndentations + "MeshFile = \"" + xtr.GetAttribute("meshFile") + "\",");
-            if(materialName != null){
-                //materialName = materialName.Insert(materialName.Length - 1, ".material");
-                lua.WriteLine(numberIndentations + "Material = " + materialName + ",");
-            }
             nSpaces -= 4;
             numberIndentations = new string(c, nSpaces);
             lua.WriteLine(numberIndentations + "}");
-        }
-
-        static Vector3 ToEulerAngles(Quaternion q)
-        {
-            Vector3 angles = new Vector3();
-
-            // roll / x
-            double sinr_cosp = 2 * (q.W * q.X + q.Y * q.Z);
-            double cosr_cosp = 1 - 2 * (q.X * q.X + q.Y * q.Y);
-            angles.X = (float)Math.Atan2(sinr_cosp, cosr_cosp);
-
-            // pitch / y
-            double sinp = 2 * (q.W * q.Y - q.Z * q.X);
-            if (Math.Abs(sinp) >= 1)
-            {
-                angles.Y = (float)Math.CopySign(Math.PI / 2, sinp);
-            }
-            else
-            {
-                angles.Y = (float)Math.Asin(sinp);
-            }
-
-            // yaw / z
-            double siny_cosp = 2 * (q.W * q.Z + q.X * q.Y);
-            double cosy_cosp = 1 - 2 * (q.Y * q.Y + q.Z * q.Z);
-            angles.Z = (float)Math.Atan2(siny_cosp, cosy_cosp);
-
-            return angles;
-        }
-
-        static Vector3 toRadians(Vector3 q){
-            q.X = (float) ((180 / Math.PI) * q.X);
-            q.Y = (float) ((180 / Math.PI) * q.Y);
-            q.Z = (float) ((180 / Math.PI) * q.Z);
-
-            return q;
         }
     }
 }
